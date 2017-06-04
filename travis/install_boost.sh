1736b000a5aa2402ff0eb9bbd9a1c90074709f57
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-06-04 15:00:28 +0200

    travis: compile boost with clang

--- travis/install_boost.sh
+++ travis/install_boost.sh
@@ -14,13 +14,13 @@ if [[ "${BOOST_VERSION}" != "" ]]; then
     if [[ "${BOOST_VERSION}" == "trunk" ]]; then
       BOOST_URL="http://github.com/boostorg/boost.git"
       travis_retry git clone --depth 1 --recursive --quiet ${BOOST_URL} ${BOOST_DIR} || exit 1
-      (cd ${BOOST_DIR} && ./bootstrap.sh && ./b2 headers)
+      (cd ${BOOST_DIR} && ./bootstrap.sh && ./b2 headers --toolset=clang)
     else
       BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//\./_}.tar.gz"
       mkdir -p ${BOOST_DIR}
       { travis_retry wget --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${BOOST_DIR}; } || exit 1
     fi
   fi
-  (cd ${BOOST_DIR} && ./bootstrap.sh --prefix=${INSTALL_DIR} && ./b2 install -q -d0 --prefix=${INSTALL_DIR} --with-program_options -j${JOBS})
+  (cd ${BOOST_DIR} && ./bootstrap.sh --prefix=${INSTALL_DIR} && ./b2 install --toolset=clang -q -d0 --prefix=${INSTALL_DIR} --with-program_options -j${JOBS})
   export PATH=${DEPS_DIR}/b2/bin:${PATH}
 fi
