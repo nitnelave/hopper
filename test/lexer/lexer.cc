9f457a0a592e5ffd8ca9872836faf1189a381be4
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-06-21 10:02:07 +0200

    lexer: add tests

---
+++ test/lexer/lexer.cc
@@ -0,0 +1,158 @@
+#include <string>
+
+#include "error/error.h"
+#include "lexer/lexer.h"
+#include "test_utils/utils.h"
+
+using lexer::Token;
+using lexer::TokenType;
+using lexer::Lexer;
+
+namespace {
+
+/// Read all the tokens from the lexer until EOF into a vector.
+/// Returns an error if one was generated at any time.
+ErrorOr<std::vector<Token>> consume_tokens(Lexer& lexer) {
+  std::vector<Token> result;
+  while (true) {
+    RETURN_OR_ASSIGN(Token tok, lexer.get_next_token());
+    if (tok.type == TokenType::END_OF_FILE) return result;
+    result.emplace_back(std::move(tok));
+  }
+  return result;
+}
+
+/// Lex a string and return the tokens.
+/// Returns an error if one was generated at any time.
+ErrorOr<std::vector<Token>> string_to_tokens(const std::string& input) {
+  Lexer lex = lexer::from_string(input);
+  return consume_tokens(lex);
+}
+
+/// Compare a list of tokens with expected types, and expected text if given.
+testing::AssertionResult compare_token_types(
+    const std::vector<Token>& tokens, const std::vector<TokenType>& types,
+    const std::vector<std::string>& text = {}) {
+  CALL_ASSERT(compare(types, MAP_VEC(tokens, __ARG__.type)));
+  if (!text.empty()) CALL_ASSERT(compare(text, MAP_VEC(tokens, __ARG__.text)));
+  return testing::AssertionSuccess();
+}
+
+/// Lex a string, then compare the tokens with expected types and text.
+testing::AssertionResult compare_token_types(
+    const std::string& input, const std::vector<TokenType>& types,
+    const std::vector<std::string>& text = {}) {
+  auto tokens_or = string_to_tokens(input);
+  if (!tokens_or.is_ok())
+    return testing::AssertionFailure() << tokens_or.error_or_die();
+  const auto& tokens = tokens_or.value_or_die();
+  return compare_token_types(tokens, types, text);
+}
+
+/// Lex a string, then compare the tokens with the expected types and the
+/// canonical representation of the types.
+testing::AssertionResult compare_operators(
+    const std::string& input, const std::vector<TokenType>& types) {
+  auto tokens_or = string_to_tokens(input);
+  if (!tokens_or.is_ok())
+    return testing::AssertionFailure() << tokens_or.error_or_die();
+  const auto& tokens = tokens_or.value_or_die();
+  return compare_token_types(tokens, types, MAP_VEC(types, to_symbol(__ARG__)));
+}
+
+}  // namespace
+
+// Tests of binary operators.
+TEST(LexerTest, BinaryOperators) {
+  EXPECT_TRUE(compare_operators(
+      "+ - / *",
+      {TokenType::PLUS, TokenType::MINUS, TokenType::DIVIDE, TokenType::STAR}));
+  EXPECT_TRUE(compare_operators(
+      "|| && > < >= <=",
+      {TokenType::OR, TokenType::AND, TokenType::GREATER, TokenType::LESS,
+       TokenType::GREATER_OR_EQUAL, TokenType::LESS_OR_EQUAL}));
+  EXPECT_TRUE(
+      compare_operators("== !=", {TokenType::EQUAL, TokenType::DIFFERENT}));
+  EXPECT_TRUE(compare_operators(
+      "|> <| | ^", {TokenType::BITSHIFT_RIGHT, TokenType::BITSHIFT_LEFT,
+                    TokenType::BITOR, TokenType::BITXOR}));
+  EXPECT_TRUE(compare_operators(
+      "= += -= *= /= |= ^= &=",
+      {TokenType::ASSIGN, TokenType::PLUS_ASSIGN, TokenType::MINUS_ASSIGN,
+       TokenType::TIMES_ASSIGN, TokenType::DIVIDE_ASSIGN, TokenType::OR_ASSIGN,
+       TokenType::XOR_ASSIGN, TokenType::AND_ASSIGN}));
+}
+
+TEST(LexerTest, BinaryOperatorsNoSpace) {
+  EXPECT_TRUE(compare_operators("+-/*", {TokenType::PLUS, TokenType::MINUS,
+                                         TokenType::DIVIDE, TokenType::STAR}));
+  EXPECT_TRUE(compare_operators(
+      "||&&><>=<=",
+      {TokenType::OR, TokenType::AND, TokenType::GREATER, TokenType::LESS,
+       TokenType::GREATER_OR_EQUAL, TokenType::LESS_OR_EQUAL}));
+  EXPECT_TRUE(
+      compare_operators("==!=", {TokenType::EQUAL, TokenType::DIFFERENT}));
+  EXPECT_TRUE(compare_operators(
+      "|><||^", {TokenType::BITSHIFT_RIGHT, TokenType::BITSHIFT_LEFT,
+                 TokenType::BITOR, TokenType::BITXOR}));
+  EXPECT_TRUE(compare_operators(
+      "=+=-=*=/=|=^=&=",
+      {TokenType::ASSIGN, TokenType::PLUS_ASSIGN, TokenType::MINUS_ASSIGN,
+       TokenType::TIMES_ASSIGN, TokenType::DIVIDE_ASSIGN, TokenType::OR_ASSIGN,
+       TokenType::XOR_ASSIGN, TokenType::AND_ASSIGN}));
+}
+
+// Tests of unary operators.
+TEST(LexerTest, UnaryOperators) {
+  EXPECT_TRUE(compare_operators(
+      "& ! ~ ++ --", {TokenType::AMPERSAND, TokenType::BANG, TokenType::TILDE,
+                      TokenType::INCREMENT, TokenType::DECREMENT}));
+  EXPECT_TRUE(compare_operators(
+      "&!~++--", {TokenType::AMPERSAND, TokenType::BANG, TokenType::TILDE,
+                  TokenType::INCREMENT, TokenType::DECREMENT}));
+  EXPECT_TRUE(
+      compare_operators("& = ! =", {TokenType::AMPERSAND, TokenType::ASSIGN,
+                                    TokenType::BANG, TokenType::ASSIGN}));
+}
+
+// Tests of numbers.
+TEST(LexerTest, Numbers) {
+  EXPECT_TRUE(compare_token_types(
+      "1 435 0x1Ab 0o745 0b100101 -3 1+1",
+      {TokenType::INT, TokenType::INT, TokenType::HEX, TokenType::OCT,
+       TokenType::BINARY_NUMBER, TokenType::MINUS, TokenType::INT,
+       TokenType::INT, TokenType::PLUS, TokenType::INT},
+      // Value in base 10 of the literals.
+      {"1", "435", "427", "485", "37", "-", "3", "1", "+", "1"}));
+}
+
+// Tests of comments.
+TEST(LexerTest, Comments) {
+  EXPECT_TRUE(compare_token_types(
+      "1+1 // Test: 4 5\n2", {TokenType::INT, TokenType::PLUS, TokenType::INT,
+                              TokenType::COMMENT, TokenType::INT},
+      {"1", "+", "1", "// Test: 4 5", "2"}));
+}
+
+// Test of location.
+TEST(LexerTest, Location) {
+  auto tokens_or = string_to_tokens("1 12 123 1234 1+1 1++1\n2");
+  ASSERT_TRUE(tokens_or.is_ok());
+  const auto& tokens = tokens_or.value_or_die();
+  std::vector<std::pair<int, int>> expected_columns = {
+      {1, 1},   {3, 4},   {6, 8},   {10, 13}, {15, 15}, {16, 16},
+      {17, 17}, {19, 19}, {20, 21}, {22, 22}, {1, 1},
+  };
+  ASSERT_EQ(expected_columns.size(), tokens.size());
+  for (unsigned int i = 0; i < expected_columns.size() - 1; ++i) {
+    EXPECT_EQ("<string>", tokens[i].begin.file);
+    EXPECT_EQ("<string>", tokens[i].end.file);
+    EXPECT_EQ(1, tokens[i].begin.line);
+    EXPECT_EQ(1, tokens[i].end.line);
+  }
+  EXPECT_EQ(2, tokens.back().begin.line);
+  EXPECT_EQ(2, tokens.back().end.line);
+  auto actual_columns = MAP_VEC(
+      tokens, (std::pair<int, int>{__ARG__.begin.column, __ARG__.end.column}));
+  EXPECT_EQ(expected_columns, actual_columns);
+}
