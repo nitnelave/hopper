e3a35ab1d38f8813f5a36a43bfe7fabf907aadc5
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-03 18:39:22 +0200

    lexer: document

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -28,19 +28,30 @@ class Lexer {
     STRING,
   };
 
+  // Build a lexer object with the given source (raw string or filename).
+  // Consider using the "from_*" family of functions.
   Lexer(const std::string& source, SourceTag tag,
         const std::string& filename = "");
 
+  // Consume characters from the stream until a full token is seen, and return
+  // that token, or a lexing error if a malformed token was seen.
   ErrorOr<Token, LexError> get_next_token();
 
+  // Get the current location, in the source, of the lexer.
   const Location& get_location() const;
 
  private:
+  // Consume a line comment (after the '//') and returns it.
   ErrorOr<Token, LexError> read_comment(const Location& beginning);
+  // Consume a number in the given base and and returns a token of type tt with
+  // the decimal value of the number in the text.
   ErrorOr<Token, LexError> read_base(const Location& beginning, TokenType tt,
                                      int base);
   // Puts the value of the next char into next_char_, and updates the location.
   void get_next_char();
+  // Setup the lexer such that the next call to get_next_char() returns the
+  // same character and location. The value of next_char_ is updated with the
+  // previous value.
   void unget_char();
 
   char next_char_ = 0;
@@ -51,7 +62,11 @@ class Lexer {
   Location previous_location_;
 };
 
+// Return an instance of Lexer that will read from the file.
 Lexer from_file(const std::string& file);
+
+// Return an instance of Lexer that will read from the given string.
+// The filename is set to "<string>".
 Lexer from_string(const std::string& text);
 
 }  // namespace lexer
