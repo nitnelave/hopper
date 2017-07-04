1edb274ca7b6dbc28927dcc1e81e274821ec92c9
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-05 00:00:14 +0200

    gflags: cleanup when exiting

--- src/main.cc
+++ src/main.cc
@@ -12,9 +12,9 @@ DEFINE_bool(success, false, "Whether we successfully added a flag");
 int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  if (FLAGS_success)
-    std::cout << "Success!\n";
+  if (FLAGS_success) std::cout << "Success!\n";
 
+  int exit_code = 0;
   for (int i = 1; i < argc; ++i) {
     const auto* input = argv[i];  // NOLINT: "pointer arithmetics"
     auto lexer = lexer::from_file(input);
@@ -22,9 +22,11 @@ int main(int argc, char* argv[]) {
     const auto result = parser.parse();
     if (!result.is_ok()) {
       std::cerr << result.to_string() << '\n';
-      exit(1);
+      exit_code = 1;
+      break;
     }
   }
 
-  return 0;
+  gflags::ShutDownCommandLineFlags();
+  return exit_code;
 }
--- test/main.cc
+++ test/main.cc
@@ -1,7 +1,11 @@
+#include <gflags/gflags.h>
+
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   int ret = RUN_ALL_TESTS();
+  gflags::ShutDownCommandLineFlags();
   return ret;
 }
