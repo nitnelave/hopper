7edf4eb9a2106e96792b246e55c06a28cc4d296c
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-04 13:47:00 +0200

    tools: add coverage tools

---
+++ tools/lcov.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+DIRNAME=$(dirname $(readlink -f "$0"))
+
+ROOT_DIR=$(dirname "${DIRNAME}")
+
+lcov --gcov-tool "${DIRNAME}/llvm-gcov" \
+  --rc lcov_branch_coverage=1 \
+  --rc lcov_excl_line="'\s*\};?\s*'" \
+  --rc lcov_excl_line="^[-0-9: #]*};?$" \
+  --no-external \
+  --directory "$ROOT_DIR" \
+  $*
