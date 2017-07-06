4199e1af20d2222d44da23c237f8f09140b46ef0
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-07 00:31:05 +0200

    clang-tidy: fix current directory

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-CURRENT_DIR=$(dirname $0)
+CURRENT_DIR=$(readlink -f $(dirname $0))
 PROJECT_DIR=$(readlink -f ${CURRENT_DIR}/..)
 BUILD=${PROJECT_DIR}/build
 
+cd ${BUILD}
 python2 ${CURRENT_DIR}/run-clang-tidy.py \
   -p $BUILD \
-  -header-filter="${PROJECT_DIR}/(src|test)"
+  -header-filter="\.\./(src|test)/.*"
