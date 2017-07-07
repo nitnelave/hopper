99b77cb9678d3077f0bde23d1808531c67d1e085
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-07 18:03:30 +0200

    pretty_printer: print variable declarations and constants

---
+++ src/pretty_printer/pretty_printer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+
+#include "ast/ast.h"
+#include "ast/int_constant.h"
+#include "ast/variable_declaration.h"
+
+namespace ast {
+class PrettyPrinterVisitor : public ASTVisitor {
+ public:
+  explicit PrettyPrinterVisitor(std::ostream& out) : out_(out) {}
+
+  void visit(VariableDeclaration* node) override {
+    out_ << (node->is_mutable() ? "mut" : "val") << ' ';
+    out_ << node->id().to_string();
+    if (node->type().is_ok())
+      out_ << " : " << node->type().value_or_die().to_string();
+    if (node->value().is_ok()) {
+      out_ << " = ";
+      node->value().value_or_die()->accept(*this);
+    }
+    out_ << ";\n";
+  }
+
+  void visit(IntConstant* node) override { out_ << node->value(); }
+
+ private:
+  std::ostream& out_;
+};
+
+}  // namespace ast
