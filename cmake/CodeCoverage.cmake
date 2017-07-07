875b8b303fffa38c92f329ba53fea2c31204acdd
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-08 01:16:32 +0200

    coverage: run c++filt on the report

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -74,10 +74,15 @@ find_program( LCOV_PATH lcov )
 find_program( GENHTML_PATH genhtml )
 find_program( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
 find_program( SIMPLE_PYTHON_EXECUTABLE python )
+find_program( CPPFILT c++filt )
 
 if(NOT GCOV_PATH)
     message(FATAL_ERROR "gcov not found! Aborting...")
 endif() # NOT GCOV_PATH
+
+if(NOT CPPFILT)
+  set(CPPFILT cat)
+endif()
 
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple)?[Cc]lang")
     if("${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS 3)
@@ -158,9 +163,10 @@ function(SETUP_TARGET_FOR_COVERAGE)
 
         # Capturing lcov counters and generating report
         COMMAND ${LCOV_PATH} ${LCOV_RC} --capture --output-file ${Coverage_NAME}.info
-        COMMAND ${LCOV_PATH} ${LCOV_RC} --remove ${Coverage_NAME}.info ${COVERAGE_EXCLUDES} --output-file ${Coverage_NAME}.info.cleaned
+        COMMAND ${LCOV_PATH} ${LCOV_RC} --remove ${Coverage_NAME}.info ${COVERAGE_EXCLUDES} --output-file ${Coverage_NAME}.info.filtered
+        COMMAND cat ${Coverage_NAME}.info.filtered | ${CPPFILT} > ${Coverage_NAME}.info.cleaned
         COMMAND ${GENHTML_PATH} -o ${Coverage_NAME} ${Coverage_NAME}.info.cleaned
-        COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.info
+        COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.info ${Coverage_NAME}.info.filtered
 
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
         DEPENDS ${Coverage_DEPENDENCIES}
