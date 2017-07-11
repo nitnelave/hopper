2250286ed3bd93875e12b0ed6348d749b1bd3a78
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-12 00:31:04 +0200

    lcov: ignore assert(false)

--- tools/lcov.sh
+++ tools/lcov.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$(dirname "${DIRNAME}")
 
 lcov --gcov-tool "${DIRNAME}/llvm-gcov" \
   --rc lcov_branch_coverage=1 \
-  --rc lcov_excl_line="(^[-0-9: #]*};?$)|(LCOV_EXCL_LINE)" \
+  --rc lcov_excl_line="(^[-0-9: #]*};?$)|(LCOV_EXCL_LINE)|(assert\(false)" \
   --no-external \
   --directory "$ROOT_DIR" \
   $*
