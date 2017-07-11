6f96dfe41694e178c45079d5d799676a041faf51
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-11 22:57:26 +0200

    token: make un-copyable

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -5,8 +5,8 @@
 #include "ast/variable_declaration.h"
 
 #define EXPECT_TOKEN(TYPE, MESSAGE)   \
-  if (current_token().type != (TYPE)) \
-    return ParseError((MESSAGE), current_token().location);
+  if (current_token().type() != (TYPE)) \
+    return ParseError((MESSAGE), current_token().location());
 
 namespace parser {
 using lexer::Token;
@@ -17,63 +17,63 @@ using ast::Type;
 Parser::Parser(Lexer* lexer) : lexer_(lexer) {}
 
 lexer::Range Parser::range_from(const Range::Position& begin) const {
-  return {current_token().location.file, begin, last_end_};
+  return {current_token().location().file, begin, last_end_};
 }
 
 ErrorOr<ast::Identifier> Parser::parse_type_identifier() {
   EXPECT_TOKEN(TokenType::UPPER_CASE_IDENT, "Expected type identifier");
-  Identifier res(current_token().text);
+  Identifier res(current_token().text());
   RETURN_IF_ERROR(get_token());
   return res;
 }
 
 ErrorOr<ast::Identifier> Parser::parse_value_identifier() {
   EXPECT_TOKEN(TokenType::LOWER_CASE_IDENT, "Expected value identifier");
-  Identifier res(current_token().text);
+  Identifier res(current_token().text());
   RETURN_IF_ERROR(get_token());
   return res;
 }
 
 ErrorOr<ast::Type> Parser::parse_type() {
-  RETURN_OR_ASSIGN(Identifier id, parse_type_identifier());
+  RETURN_OR_MOVE(Identifier id, parse_type_identifier());
   return Type(id);
 }
 
 Parser::ErrorOrPtr<ast::IntConstant> Parser::parse_int_constant() {
-  auto location = current_token().location;
-  auto value = std::atoi(current_token().text.c_str());
+  auto location = current_token().location();
+  auto value = current_token().int_value();
   RETURN_IF_ERROR(get_token());
   return std::make_unique<ast::IntConstant>(location, value);
 }
 
 Parser::ErrorOrPtr<ast::Value> Parser::parse_value() {
-  if (current_token().type == TokenType::INT ||
-      current_token().type == TokenType::HEX ||
-      current_token().type == TokenType::OCT ||
-      current_token().type == TokenType::BINARY_NUMBER)
+  if (current_token().type() == TokenType::INT ||
+      current_token().type() == TokenType::HEX ||
+      current_token().type() == TokenType::OCT ||
+      current_token().type() == TokenType::BINARY_NUMBER)
     return parse_int_constant();
-  return ParseError("Expected value", current_token().location);
+  return ParseError("Expected value", current_token().location());
 }
 
 Parser::ErrorOrPtr<ast::VariableDeclaration>
 Parser::parse_variable_declaration() {
   // Starts with VAL or MUT.
-  assert(current_token().type == TokenType::VAL ||
-         current_token().type == TokenType::MUT);
-  auto begin = current_token().location.begin;
-  bool mut = current_token().type == TokenType::MUT;
+  assert(current_token().type() == TokenType::VAL ||
+         current_token().type() == TokenType::MUT);
+  bool mut = current_token().type() == TokenType::MUT;
+  auto begin = current_token().location().begin;
   RETURN_IF_ERROR(get_token());
   // Then a name, lowercase.
-  RETURN_OR_ASSIGN(Identifier variable_name, parse_value_identifier());
+  RETURN_OR_MOVE(Identifier variable_name, parse_value_identifier());
   // Then an optional type.
   Option<Type> type;
-  if (current_token().type == TokenType::COLON) {
+  if (current_token().type() == TokenType::COLON) {
     RETURN_IF_ERROR(get_token());
-    RETURN_OR_ASSIGN(type, parse_type());
+    RETURN_OR_MOVE(type, parse_type());
   }
   // Then an optional value.
   Option<std::unique_ptr<ast::Value>> value;
-  if (current_token().type == TokenType::ASSIGN) {
+  if (current_token().type() == TokenType::ASSIGN) {
     RETURN_IF_ERROR(get_token());
     RETURN_OR_MOVE(value, parse_value());
   }
@@ -86,20 +86,20 @@ Parser::parse_variable_declaration() {
 }
 
 Parser::ErrorOrPtr<ast::ASTNode> Parser::parse_toplevel_declaration() {
-  const Token& t = current_token();
-  if (t.type == TokenType::VAL || t.type == TokenType::MUT) {
+  if (current_token().type() == TokenType::VAL ||
+      current_token().type() == TokenType::MUT) {
     return parse_variable_declaration();
   }
-  return ParseError("Expected top-level declaration", current_token().location);
+  return ParseError("Expected top-level declaration", current_token().location());
 }
 
 MaybeError<> Parser::get_token() {
   if (!token_stack_.empty()) {
-    last_end_ = current_token().location.end;
+    last_end_ = current_token().location().end;
   }
   do {
     RETURN_IF_ERROR(token_stack_.get_next());
-  } while (current_token().type == TokenType::COMMENT);
+  } while (current_token().type() == TokenType::COMMENT);
   return {};
 }
 
@@ -109,9 +109,9 @@ const Token& Parser::current_token() const { return token_stack_.current(); }
 
 Parser::ErrorOrPtr<ast::Module> Parser::parse() {
   RETURN_IF_ERROR(get_token());
-  auto begin = current_token().location.begin;
+  auto begin = current_token().location().begin;
   std::vector<std::unique_ptr<ast::ASTNode>> declarations;
-  while (current_token().type != TokenType::END_OF_FILE) {
+  while (current_token().type() != TokenType::END_OF_FILE) {
     RETURN_OR_MOVE(auto decl, parse_toplevel_declaration());
     declarations.emplace_back(std::move(decl));
   }
