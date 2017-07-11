9142d33f821cfbdb20c4662a6ea7ff9bbecb4272
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-11 23:21:14 +0200

    parser: add support for variable reference

---
+++ src/ast/variable_reference.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ast/ast.h"
+#include "ast/base_types.h"
+#include "ast/value.h"
+
+namespace ast {
+
+class VariableReference : public Value {
+ public:
+  VariableReference(lexer::Range location, Identifier id)
+      : Value(std::move(location)), id_(std::move(id)) {}
+
+  const Identifier& id() const { return id_; }
+
+  ~VariableReference() override = default;
+
+ private:
+  void accept_impl(ASTVisitor& visitor) override { visitor.visit(this); }
+
+  Identifier id_;
+};
+
+}  // namespace ast
