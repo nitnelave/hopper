692dda00c835a70ef5a91e8c878d2834a0ef4550
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-19 17:25:48 +0200

    parser: parse function calls

---
+++ src/ast/function_call.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "ast/base_types.h"
+#include "ast/value.h"
+
+namespace ast {
+class FunctionCall : public Value {
+ public:
+  using ArgumentList = std::vector<std::unique_ptr<Value>>;
+  FunctionCall(lexer::Range range, std::unique_ptr<Value> base,
+               ArgumentList args)
+      : Value(std::move(range)),
+        base_(std::move(base)),
+        args_(std::move(args)) {}
+
+  // The base of the function, or what comes before the arguments.
+  Value& base() { return *base_; }
+
+  ArgumentList& arguments() { return args_; }
+
+ private:
+  void accept_impl(ASTVisitor& visitor) override { visitor.visit(this); }
+  std::unique_ptr<Value> base_;
+  ArgumentList args_;
+};
+}  // namespace ast
