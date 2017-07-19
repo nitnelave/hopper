3775e9048ecc00d4d49437ab658cace32d90a399
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-19 14:57:31 +0200

    pretty_printer: move function declaration print to a file

--- src/pretty_printer/pretty_printer.h
+++ src/pretty_printer/pretty_printer.h
@@ -28,35 +28,7 @@ class PrettyPrinterVisitor : public ASTVisitor {
     out_ << ";\n";
   }
 
-  void visit(FunctionDeclaration* node) override {
-    print_indent() << "fun " << node->id().to_string() << '(';
-    bool print_comma = false;
-    for (const auto& arg : node->arguments()) {
-      if (print_comma) out_ << ',';
-      arg->accept(*this);
-    }
-    out_ << ')';
-    if (node->type().is_ok())
-      out_ << " : " << node->type().value_or_die().to_string();
-    out_ << ' ';
-    if (node->body().is<FunctionDeclaration::StatementsBody>()) {
-      out_ << "{\n";
-      ++indent_;
-      for (const auto& statement :
-           node->body().get_unchecked<FunctionDeclaration::StatementsBody>()) {
-        statement->accept(*this);
-        out_ << '\n';
-      }
-      --indent_;
-      print_indent() << "}";
-    } else {
-      out_ << "= ";
-      node->body().get_unchecked<FunctionDeclaration::ValueBody>()->accept(
-          *this);
-      out_ << ';';
-    }
-    out_ << '\n';
-  }
+  void visit(FunctionDeclaration* node) override;
   void visit(BinaryOp* node) override;
 
   void visit(VariableReference* node) override {
