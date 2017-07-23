658a60e2c829e0fff4113921e891b3db7ae4bfbb
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-07-23 17:42:16 +0200

    ast: add Declaration and TypeDeclaration meta-nodes

--- src/ast/function_declaration.h
+++ src/ast/function_declaration.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "ast/base_types.h"
+#include "ast/declaration.h"
 #include "ast/function_argument_declaration.h"
 #include "ast/statement.h"
 #include "ast/value.h"
@@ -12,7 +13,7 @@
 
 namespace ast {
 
-class FunctionDeclaration : public ASTNode {
+class FunctionDeclaration : public Declaration {
  public:
   using ArgumentList =
       std::vector<std::unique_ptr<FunctionArgumentDeclaration>>;
@@ -21,23 +22,15 @@ class FunctionDeclaration : public ASTNode {
   FunctionDeclaration(lexer::Range location, Identifier id,
                       ArgumentList arguments, Option<Type> type,
                       StatementsBody body)
-      : ASTNode(std::move(location)),
-        id_(std::move(id)),
+      : Declaration(std::move(location), std::move(id), std::move(type)),
         arguments_(std::move(arguments)),
-        type_(std::move(type)),
         body_(std::move(body)) {}
 
   FunctionDeclaration(lexer::Range location, Identifier id,
                       ArgumentList arguments, Option<Type> type, ValueBody body)
-      : ASTNode(std::move(location)),
-        id_(std::move(id)),
+      : Declaration(std::move(location), std::move(id), std::move(type)),
         arguments_(std::move(arguments)),
-        type_(std::move(type)),
         body_(std::move(body)) {}
-
-  const Identifier& id() const { return id_; }
-
-  const Option<Type>& type() const { return type_; }
 
   const ArgumentList& arguments() const { return arguments_; }
 
@@ -57,9 +50,7 @@ class FunctionDeclaration : public ASTNode {
  private:
   void accept_impl(ASTVisitor& visitor) override { visitor.visit(this); }
 
-  Identifier id_;
   ArgumentList arguments_;
-  Option<Type> type_;
   Variant<StatementsBody, ValueBody> body_;
 };
 
