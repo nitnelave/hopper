7c9eafd7f554037f4a23323201fbcf8e04f6a28a
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-08-01 22:44:30 +0200

    logging: expand interface, add check

--- src/util/logging.h
+++ src/util/logging.h
@@ -7,7 +7,7 @@
 DECLARE_int32(verbosity);
 
 enum Severity {
-  ERROR = 1,
+  ERROR = 0,
   WARNING,
   INFO,
   DEBUG,
@@ -19,10 +19,10 @@ class Logger {
  public:
   Logger(const Logger&) = delete;
   Logger& operator=(const Logger&) = delete;
-  Logger(Logger&&) = default;
-  Logger& operator=(Logger&&) = default;
-  Logger(std::ostream& out, bool should_print)
-      : should_print_(should_print), out_(out) {}
+  Logger(Logger&&) noexcept = default;
+  Logger& operator=(Logger&&) noexcept = default;
+  Logger(std::ostream& out, bool should_print, bool is_fatal = false)
+      : should_print_(should_print), is_fatal_(is_fatal), out_(out) {}
 
   template <typename T>
   const Logger& print(const T& value) const {
@@ -32,10 +32,14 @@ class Logger {
     return *this;
   }
 
-  ~Logger() { print('\n'); }
+  ~Logger() {
+    print('\n');
+    if (is_fatal_) std::exit(1);
+  }
 
  private:
   bool should_print_;
+  bool is_fatal_;
   std::ostream& out_;
 };
 
@@ -45,5 +49,22 @@ const Logger& operator<<(const Logger& out, const T& value) {
 }
 }  // namespace internals
 
+void initialize_logging();
+
 internals::Logger log(Severity s);
 internals::Logger log_if(Severity s, bool condition);
+internals::Logger no_log();
+
+internals::Logger log_fatal();
+
+internals::Logger check_or_die(bool condition);
+
+#define CHECK(CONDITION)                                \
+  check_or_die((CONDITION)) << __FILE__ ":" << __LINE__ \
+                            << ": Check `" #CONDITION "' failed. "
+
+#ifndef NDEBUG
+#define DCHECK(CONDITION) CHECK(CONDITION)
+#else
+#define DCHECK(CONDITION) no_log()
+#endif
