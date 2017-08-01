0e5312bf7c3f79f8b502e3c84183bbbf822973f2
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-08-01 22:45:58 +0200

    gflags: improve help message

--- src/util/gflags_utils.h
+++ src/util/gflags_utils.h
@@ -4,6 +4,10 @@
 
 #include "util/logging.h"
 
+DECLARE_bool(h);
+DECLARE_bool(help);
+DECLARE_string(helpmatch);
+
 namespace gflags {
 
 // LCOV_EXCL_START
@@ -13,7 +17,17 @@ namespace gflags {
 class GFlagsWrapper {
  public:
   GFlagsWrapper(int* argc, char*** argv, bool keep_rest) {
-    ParseCommandLineFlags(argc, argv, keep_rest);
+    ParseCommandLineNonHelpFlags(argc, argv, keep_rest);
+    // Override --help to show only flags defined in gHopper.
+    if (FLAGS_help || FLAGS_h) {
+      FLAGS_help = false;
+      // This value is a hack that depends on the fact that the build directory
+      // is one folder under the root of the project.
+      // If we need a nicer help message, we need to change library or fork
+      // gflags.
+      FLAGS_helpmatch = "../src/";
+    }
+    gflags::HandleCommandLineHelpFlags();
     initialize_logging();
   }
 
