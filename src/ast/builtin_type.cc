72b291a3029a6a288e8d12a81c347171fc94bcab
Vincent Latrouite <v.latrouite@criteo.com>
2017-08-03 16:37:55 +0100

    Add default value for width_to_int_type + tests

--- src/ast/builtin_type.cc
+++ src/ast/builtin_type.cc
@@ -1,4 +1,5 @@
 #include "ast/builtin_type.h"
+
 namespace ast {
 namespace internals {
 const lexer::Range builtin_range{"<builtin>", {0, 0}, {0, 0}};
@@ -23,6 +24,7 @@ Option<IntWidth> int_type_to_width(const TypeDeclaration* type) {
   if (type == &int32) return IntWidth::W_32;
   if (type == &int16) return IntWidth::W_16;
   if (type == &int8) return IntWidth::W_8;
+
   return none;
 }
 
@@ -37,6 +39,8 @@ const TypeDeclaration* width_to_int_type(IntWidth width) {
     case IntWidth::W_64:
       return &int64;
   }
+
+  assert(false && "Unreachable code");
 }
 }  // namespace types
 }  // namespace ast
