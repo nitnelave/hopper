c60a0755fa58c81c93ada46ef7119c65c9a05028
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-08-05 00:19:15 +0200

    logging: add documentation

--- src/util/logging.cc
+++ src/util/logging.cc
@@ -13,6 +13,8 @@ DEFINE_bool(warnings, false, "Set verbosity to warning (1)");
 DEFINE_bool(verbose, false, "Set verbosity to info (2)");
 DEFINE_bool(debug, false, "Set verbosity to debug (3)");
 
+using logging::Logger;
+
 namespace {
 bool validate_verbosity(const char* flag_name, gflags::int32 value) {
   if (value < ERROR || value > DEBUG) {
@@ -21,35 +23,34 @@ bool validate_verbosity(const char* flag_name, gflags::int32 value) {
   }
   return true;
 }
-
 }  // namespace
 
 DEFINE_validator(verbosity, &validate_verbosity);
 
+namespace logging {
 void initialize_logging() {
   if (FLAGS_quiet) gflags::SetCommandLineOption("verbosity", "0");
   if (FLAGS_warnings) gflags::SetCommandLineOption("verbosity", "1");
   if (FLAGS_verbose) gflags::SetCommandLineOption("verbosity", "2");
   if (FLAGS_debug) gflags::SetCommandLineOption("verbosity", "3");
 }
+}  // namespace logging
 
-// log(INFO) << "Just a joke";
-// log_if(INFO, a > 3) << "Nothing much";
+Logger log(Severity s) { return log_if(s, true); }
 
-internals::Logger log(Severity s) { return log_if(s, true); }
+Logger no_log() { return log_if(INFO, false); }
 
-internals::Logger no_log() { return log_if(INFO, false); }
-
-internals::Logger log_if(Severity s, bool condition) {
-  return internals::Logger(FLAGS_logtostderr ? std::cerr : std::cout,
+Logger log_if(Severity s, bool condition) {
+  return Logger(FLAGS_logtostderr ? std::cerr : std::cout,
                            s <= FLAGS_verbosity && condition);
 }
 
-internals::Logger log_fatal() {
-  return internals::Logger(std::cerr, true, true);
+Logger log_fatal() {
+  return Logger(std::cerr, true, true);
 }
 
-internals::Logger check(bool condition) {
-  return internals::Logger(std::cerr, !condition, !condition);
+Logger check(bool condition) {
+  // If condition is false, print messages and then crash.
+  return Logger(std::cerr, !condition, !condition);
 }
 // LCOV_EXCL_STOP
--- src/util/logging.h
+++ src/util/logging.h
@@ -1,39 +1,62 @@
 #pragma once
 
+/// This file contains the logging utilities.
+///
+/// It is highly discourage to write directly to std::cerr or std::cout.
+/// Instead, use the logging utilities.
+///
+/// Example:
+/// log(INFO) << "Answer to the universe: " << 42;
+/// log_if(WARNING, a > 9000) << "It's over 9000!";
+/// CHECK(K >= 0) << "Oh, no! The temperature went under absolute 0!";
+
 #include <ostream>
 
 #include "gflags/gflags.h"
 
 // LCOV_EXCL_START: logging is not tested
 
+/// This flag controls the verbosity of the messages to be printed.
 DECLARE_int32(verbosity);
 
+/// This enum controls the verbosity setting at which a message will appear.
 enum Severity {
+  /// Something happened that was not supposed to. If this is printed, a bug
+  /// report should be submitted.
   ERROR = 0,
+  /// You're doing something dangerous or insecure, but it still should work.
+  /// Don't blame us if it doesn't, though, you were warned.
+  /// Default verbosity level.
   WARNING,
+  /// High-level information, about the stages of compilation, that developpers
+  /// should see.
   INFO,
+  /// Detailed messages about the implementation.
   DEBUG,
 };
 
-namespace internals {
+namespace logging {
 
+/// RAII printer. This object is a lightweight wrapper around an ostream.
 class Logger {
  public:
+  /// No copy
   Logger(const Logger&) = delete;
   Logger& operator=(const Logger&) = delete;
+  /// Move is okay.
   Logger(Logger&&) noexcept = default;
   Logger& operator=(Logger&&) noexcept = default;
+  /// Constructor.
   Logger(std::ostream& out, bool should_print, bool is_fatal = false)
       : should_print_(should_print), is_fatal_(is_fatal), out_(out) {}
 
   template <typename T>
   const Logger& print(const T& value) const {
-#ifndef NDEBUG
     if (should_print_) out_ << value;
-#endif
     return *this;
   }
 
+  /// On destruction, print a new line, and exit (if relevant).
   ~Logger() {
     print('\n');
     if (is_fatal_) std::exit(1);
@@ -49,22 +72,39 @@ template <typename T>
 const Logger& operator<<(const Logger& out, const T& value) {
   return out.print(value);
 }
-}  // namespace internals
 
+/// Handle logging flags. Has to be called after flag parsing, but before any
+/// logging.
 void initialize_logging();
+}  // namespace logging
 
-internals::Logger log(Severity s);
-internals::Logger log_if(Severity s, bool condition);
-internals::Logger no_log();
+/// Print messages with the given Severity.
+logging::Logger log(Severity s);
+/// Print messages if the condition is fulfilled. The condition is evaluated
+/// once before the messages, and is guaranteed to be evaluated.
+logging::Logger log_if(Severity s, bool condition);
+/// Dummy logging object that doesn't print anything.
+logging::Logger no_log();
 
-internals::Logger log_fatal();
+/// Log a message with Severity ERROR, and then exit the program. Prefer to use
+/// the macro LOG_FATAL.
+logging::Logger log_fatal();
 
-internals::Logger check_or_die(bool condition);
+/// Macro for better reporting of fatal errors.
+#define LOG_FATAL \
+  log_fatal() << __FILE__ ":" << __LINE__ \
+                            << ": Fatal error. "
+/// Evaluate the condition, and if it is false, then print the message and exit
+/// the program. Prefer to use the macro CHECK.
+logging::Logger check_or_die(bool condition);
 
+/// Macro for better reporting of failed checks.
 #define CHECK(CONDITION)                                \
   check_or_die((CONDITION)) << __FILE__ ":" << __LINE__ \
                             << ": Check `" #CONDITION "' failed. "
 
+/// Debug asserts. Only compiled in debug mode. In release mode, the condition
+/// is not evaluated.
 #ifndef NDEBUG
 #define DCHECK(CONDITION) CHECK(CONDITION)
 #else
