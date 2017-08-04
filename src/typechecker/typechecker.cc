57d4091b290448a65df06afb9f061977d7e422d1
Valentin Tolmer <valentin.tolmer@gmail.com>
2017-08-04 12:22:30 +0200

    typechecker: implement function return type

--- src/typechecker/typechecker.cc
+++ src/typechecker/typechecker.cc
@@ -6,6 +6,7 @@
 #include "ast/builtin_type.h"
 #include "ast/function_declaration.h"
 #include "ast/int_constant.h"
+#include "ast/return_statement.h"
 #include "ast/variable_reference.h"
 
 namespace typechecker {
@@ -60,9 +61,9 @@ bool is_integer_operator(ast::BinaryOperator op) {
 
 void TypeChecker::visit(ast::VariableReference* node) {
   assert(node->is_resolved() && "Variable was not resolved");
-  const auto& declaration = node->resolution().value_or_die()->type();
-  assert(declaration.is_ok() && "Declaration did not have a type");
-  node->type() = Type(declaration.value_or_die().get_declaration());
+  const auto& declaration_type = node->resolution().value_or_die()->type();
+  assert(declaration_type.is_ok() && "Declaration did not have a type");
+  node->type() = Type(declaration_type.value_or_die().get_declaration());
 }
 
 void TypeChecker::visit(ast::BooleanConstant* node) {
@@ -85,10 +86,11 @@ void TypeChecker::visit(ast::BinaryOp* node) {
   const auto& right_type = node->right_value().type().value_or_die();
   if (is_integer_operator(node->operation()) && is_integer(left_type) &&
       is_integer(right_type)) {
-    using namespace ast::types;
-    IntWidth result_width = std::max(
-        int_type_to_width(left_type.get_declaration()).value_or_die(),
-        int_type_to_width(right_type.get_declaration()).value_or_die());
+    ast::types::IntWidth result_width =
+        std::max(ast::types::int_type_to_width(left_type.get_declaration())
+                     .value_or_die(),
+                 ast::types::int_type_to_width(right_type.get_declaration())
+                     .value_or_die());
     node->type() = width_to_int_type(result_width);
   } else if (is_boolean_operator(node->operation()) && is_boolean(left_type) &&
              is_boolean(right_type)) {
@@ -102,18 +104,49 @@ void TypeChecker::visit(ast::BinaryOp* node) {
   }
 }
 
+void TypeChecker::visit(ast::ReturnStatement* node) {
+  size_t num_errors = error_list_.errors().size();
+  ASTVisitor::visit(node);
+  if (num_errors < error_list_.errors().size()) return;
+
+  const Type value_type = [&]() {
+    if (node->value().is_ok()) {
+      const auto& maybe_type = node->value().value_or_die()->type();
+      assert(maybe_type.is_ok());
+      return maybe_type.value_or_die();
+    }
+    return Type(&ast::types::void_type);
+  }();
+  if (function_return_type_.is_ok()) {
+    if (function_return_type_.value_or_die() != value_type) {
+      error_list_.add_error(
+          node->location(),
+          "Invalid return type: the function returns `" +
+              function_return_type_.value_or_die().to_string() +
+              "', but the return value is of type `" + value_type.to_string() +
+              "'");
+      return;
+    }
+  }
+  function_return_type_ = value_type;
+}
+
 void TypeChecker::visit(ast::FunctionDeclaration* node) {
   // Assumes we ran the FunctionValueBody transformer first.
   size_t num_errors = error_list_.errors().size();
-  for (auto& statement :
-       node->body().get<ast::FunctionDeclaration::StatementsBody>())
-    statement->accept(*this);
+
+  function_return_type_ = node->type();
+  // Visit the children.
+  ASTVisitor::visit(node);
   if (num_errors < error_list_.errors().size())
     // Errors while processing the body.
     return;
   // Visit all the statements, looking for return statements, collect the
   // types.
   if (node->type().is_ok()) return;
+  if (!function_return_type_.is_ok())
+    function_return_type_ = Type(&ast::types::void_type);
+  node->type() = function_return_type_;
 }
 
 }  // namespace typechecker
--- src/typechecker/typechecker.h
+++ src/typechecker/typechecker.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "ast/ast.h"
+#include "ast/base_types.h"
 #include "error/error.h"
 #include "lexer/token.h"
 #include "visitor/error_visitor.h"
@@ -18,8 +19,12 @@ class TypeChecker : public ast::ASTVisitor {
   void visit(ast::IntConstant* node) override;
   void visit(ast::BinaryOp* node) override;
   void visit(ast::FunctionDeclaration* node) override;
+  void visit(ast::ReturnStatement* node) override;
+  void visit(ast::VariableReference* node) override;
 
  private:
+  // We may have to turn that into a stack to support nested functions.
+  Option<ast::Type> function_return_type_ = none;
   ErrorList error_list_;
 };
 }  // namespace typechecker
